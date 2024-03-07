dbb1ceb18813981ecd5ee6b9433082fcc40778df
yannarmstrong <yann.armstrong@stu.ocsb.ca>
2024-03-07 14:26:06 -0500

    Finishing

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function () {
     const darkModeToggle = document.getElementById('dark-mode-toggle');
     const darkModeCheckbox = document.getElementById('dark-mode-checkbox');
     
-    // Check the saved preference in local storage
     if (localStorage.getItem('darkMode') === 'enabled') {
         document.body.classList.add('dark-mode');
         darkModeCheckbox.checked = true;
     }
 
-    // Toggle dark mode when checkbox is clicked
     darkModeToggle.addEventListener('change', function () {
         if (darkModeCheckbox.checked) {
             document.body.classList.add('dark-mode');
